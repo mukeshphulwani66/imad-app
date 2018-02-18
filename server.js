fb4867a94c6147d22f0e54442b8c95cb7193b9a5
mukeshphulwani66 <35360209+mukeshphulwani66@users.noreply.github.com>
2018-02-18 13:20:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-app.use(express.static(path.join(__dirname , 'ui')));
+app.use(express.static(__dirname + 'ui'));
 var articles = {
     articleone:{
      heading:"i am heading one",
