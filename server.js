e45120231203888d195059a52d2d8d4c7c6016ef
mukeshphulwani66 <35360209+mukeshphulwani66@users.noreply.github.com>
2018-02-18 13:25:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -68,7 +68,7 @@ app.get('/ui/style.css', function (req, res) {
 //   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 // });
 
-app.use(express.static('ui'));
+app.use('/static',express.static('ui'));
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
