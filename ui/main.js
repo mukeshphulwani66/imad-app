9fc2e5736f6d7e31933c83bd5e49933218112ad8
mukeshphulwani66 <35360209+mukeshphulwani66@users.noreply.github.com>
2018-02-18 14:07:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,3 +20,20 @@ button.onclick = function(){
   Request.send(null);
   
 }
+
+//submit name
+var nameinput =  document.getElementById("name");
+var name = nameinput.value;
+var submit =  document.getElementById("submit_btn");
+submit.onclick = function(){
+    
+var names = ['name1','name2','name2'];
+var list = "";
+fot(var i = 0; i<names.length; i++){
+    
+    list += '<li>'+names[0] +'</li>';
+}
+    
+    var ul = document.getElementById("namelist");
+    ui.innerHTML =list;
+}
