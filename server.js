f89ab1973b9aec994837d62f80e3875fd6d0b5be
mukeshphulwani66 <35360209+mukeshphulwani66@users.noreply.github.com>
2018-02-18 12:32:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.get('/', function (req, res) {
 var counter = 0;
 app.get('/counter',function(req,res){
   counter = counter+1;
-  res.send(counter);
+  res.send(counter.toString());
     
 });
 
