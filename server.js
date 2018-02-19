94c0fb264e485126ab7b7b92983c652327aae44f
mukeshphulwani66 <35360209+mukeshphulwani66@users.noreply.github.com>
2018-02-19 18:53:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -67,9 +67,9 @@ app.get('/:articlename',function(req,res){
    res.send(createtamplate(articles[articlename])); 
 });
 
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
+// app.get('/ui/style.css', function (req, res) {
+//   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+// });
 
  app.get('/ui/main.js', function (req, res) {
    res.sendFile(path.join(__dirname, 'ui', 'main.js'));
