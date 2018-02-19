7c25d98fd661573306aa6a53c3b6e8cc1a4ed20e
mukeshphulwani66 <35360209+mukeshphulwani66@users.noreply.github.com>
2018-02-19 19:06:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-app.use(express.static(__dirname + 'ui'));
+app.use(express.static('ui'));
 
 var articles = {
     articleone:{
