56106ed92f87a78a5aecc5deb6b5717e787677ea
mukeshphulwani66 <35360209+mukeshphulwani66@users.noreply.github.com>
2018-02-20 22:25:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
 
+var config= {
+    user:'mukeshphulwani66',
+    database:'mukeshphulwani',
+    host:'http://db.imad.hasura-app.io',
+    port:'5432',
+    password:process.env.DB_PASSWORD
+    
+}
 var app = express();
 app.use(morgan('combined'));
 
@@ -44,6 +53,21 @@ return htmltemplate;
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+var pool = new Pool(config);
+app.get('/test-db',function(){
+   //make a select request 
+   //return a responce with result
+   
+   pool.query('SELECT * FROM test',function(err,result){
+      if(err){
+          res.status(500).send(err.toString());
+      } else {
+          res.send(JSON.stringify(result));
+      }
+   });
+   
 });
 
 var counter = 0;
